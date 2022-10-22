dae52584a2faa3fdd89f40eafa9630c8d3e2a507
nehabalaji <neha.balaji@mutualmobile.com>
2022-10-22 16:51:52 +0530

    add placeholder image

--- app/src/main/java/com/example/rickandmortyapp/adapter/BindingAdapter.kt
+++ app/src/main/java/com/example/rickandmortyapp/adapter/BindingAdapter.kt
@@ -4,15 +4,19 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.RoundedCornersTransformation
+import com.example.rickandmortyapp.R
 
 @BindingAdapter("loadImageFromUrl")
 fun ImageView.loadImage(characterPhoto: String?) {
     this.load(characterPhoto) {
         transformations(RoundedCornersTransformation(30f))
+        placeholder(R.drawable.rick_and_morty_placeholder)
     }
 }
 
 @BindingAdapter("loadImageFromUrlWithoutTransformation")
 fun ImageView.loadImageWithoutTransformation(characterPhoto: String?) {
-    this.load(characterPhoto)
+    this.load(characterPhoto) {
+        placeholder(R.drawable.rick_and_morty_placeholder)
+    }
 }
